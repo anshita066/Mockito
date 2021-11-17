1c27ffcdb28d63d4e3cd753ea11fbb4bd01b93b2
anshita066 <anshita_066@yahoo.in>
2021-11-17 17:02:47 +0530

    committing

---
+++ app/src/test/java/com/example/mockito/MainFragmentTest.kt
@@ -0,0 +1,22 @@
+package com.example.mockito
+
+import android.content.Context
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.runners.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class MainFragmentTest {
+    lateinit var mockeditemclick :MainFragment
+    @Mock
+    lateinit var context: Context
+
+
+
+    @Before
+    fun setup(){
+//        context = Instrumentation
+    }
+}
